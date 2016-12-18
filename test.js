d797e6dfa35a3e85384c564b4d79bdeca1a64e95
thenewstockton <phillip56@hotmail.com>
2016-12-19 00:31:50 +0800

    react-skeleton

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
 //npm install readline-sync
+//npm init
+//npm install -g browserify
+//npm install --save babelify
+//npm install --save watchify
+//npm install --save babel-preset-react
+//npm install --save react
+//npm install --save react-dom
 var rs = require("readline-sync");
 
 var name = rs.question("what is your name?");
